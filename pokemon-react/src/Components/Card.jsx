4be5a3d32671c4e0f25c5919093378ced7588a95
PAOLA MARIA garrido <you@example.com>
2025-03-30 16:10:52 -0600

    Initial UI design

---
+++ pokemon-react/src/Components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card=()=>{
+    return(
+        <>
+            <div className="card">
+                <h3>1</h3>
+                <img src="./images/charmander.png" alt=""/>
+                <h2>Charmander</h2>
+            </div>
+        </>
+    )
+}
+
+export default Card;
--- pokemon-react/src/Components/Main.jsx
+++ pokemon-react/src/Components/Main.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
 
 const Main=()=>{
     return(
         <>
             <div className="container">
                 <div className="left-content">
-
+                    <div className="card-grid">
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                        <Card />
+                    </div>
+                    <div className="btn-group">
+                        <button>Previous</button>
+                        <button>Next</button>
+                    </div>
                 </div>
                 <div className="right-content">
-
+                    <Pokeinfo/>
                 </div>
             </div>
         </>
